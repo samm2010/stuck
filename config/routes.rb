23f4040b89a89af9bfbe6c3ae8726e22bc31e5b8
Osama Moazzam <sammg2010@hotmaiL.com>
2016-11-09 18:38:52 +0500

    added profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  
   resources :articles
   devise_for :users
+  
   root to: 'pages#home'
-
+resources :users , only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
