b7f9859791b541e738a275080db79f75b26d3f0c
Osama Moazzam <sammg2010@hotmaiL.com>
2016-11-11 12:29:53 +0500

    added users index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_for :users,controllers:{registrations: "registrations"}
   
   root to: 'pages#home'
-resources :users , only: [:show]
+resources :users , only: [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
