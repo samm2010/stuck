7956e0a8a9b7f4bda2ad66ec4746077c1e7dcba5
Osama Moazzam <sammg2010@hotmaiL.com>
2016-11-11 13:28:43 +0500

    added dummy database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "sam1@test.com", password: "123456", password_confirmation: "123456")
+User.create(email: "sam12@test.com", password: "123456", password_confirmation: "123456")
+User.create(email: "sam12222@test.com", password: "123456", password_confirmation: "123456")
+User.create(email: "sam122@test.com", password: "123456", password_confirmation: "123456")
+p "Test users created"
